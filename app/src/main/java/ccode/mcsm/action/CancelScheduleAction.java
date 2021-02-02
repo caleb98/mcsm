d2ddb089f51e71c6670ad0450f0c14af8e857fa0
Caleb Cassady <caleb.cassady@hotmail.com>
2021-02-01 21:49:47 -0600

    Added permission checking to the CancelSchedule action

--- app/src/main/java/ccode/mcsm/action/CancelScheduleAction.java
+++ app/src/main/java/ccode/mcsm/action/CancelScheduleAction.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import ccode.mcsm.MinecraftServerManager;
 import ccode.mcsm.permissions.Permissions;
 import ccode.mcsm.permissions.Player;
+import ccode.mcsm.scheduling.Schedule;
 import ccode.mcsm.scheduling.Scheduler;
 
 public class CancelScheduleAction extends Action {
@@ -20,6 +21,13 @@ public class CancelScheduleAction extends Action {
 		Set<String> schedules = Scheduler.getSchedules();
 		if(!schedules.contains(args)) {
 			sendMessage(manager, executor, "Error: schedule not found.");
+			return -1;
+		}
+		
+		//Make sure that the schedule is allowed to be cancelled by this user.
+		Schedule schedule = Scheduler.getSchedule(args);
+		if(schedule.executor.getPermissionsLevel() > executor.getPermissionsLevel()) {
+			sendMessage(manager, executor, "The permission level of the user who started this schedule is higher than yours. You may not cancel it.");
 			return -1;
 		}
 		
